131bd23f40231a4874bc6d2d65499ebd721ec1ad
olegweb1981 <omykhaylovskiy@gmail.com>
2020-02-24 14:36:36 +0200

    add submenu

--- js/index.js
+++ js/index.js
@@ -3,3 +3,11 @@
 function toggleMenu() {
   $(document.body).toggleClass('navbar-hidden');
 }
+
+function toggleSubmenu() {
+  document.getElementById("submenu").classList.toggle("open");
+}
+
+function toggleAnotherSubmenu() {
+  document.getElementById("another-submenu").classList.toggle("open");
+}
